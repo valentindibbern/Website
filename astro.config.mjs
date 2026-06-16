d946174b4459583b965f850bb6e2813f29455278
Valentin Dibbern <valentindibbern@users.noreply.github.com>
2026-06-16 14:16:12 +0200

    removed unused dependencies and reset project to minimal configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,13 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
-import markdoc from "@astrojs/markdoc";
-import mdx from "@astrojs/mdx";
-
-// bejamas:astro-fonts:start
-/** @type {NonNullable<import("astro/config").AstroUserConfig["fonts"]>} */
-const BEJAMAS_ASTRO_FONTS = [
-  {
-    provider: fontProviders.google(),
-    name: "Inter",
-    cssVariable: "--font-sans",
-    subsets: ["latin"],
-  },
-];
-// bejamas:astro-fonts:end
 
 // https://astro.build/config
 export default defineConfig({
-	fonts: BEJAMAS_ASTRO_FONTS,
     site: "https://www.example.com",
 
-    integrations: [alpinejs(), markdoc(), mdx()],
+    integrations: [alpinejs()],
 
     vite: {
         plugins: [tailwindcss()],
